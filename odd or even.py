3c568df1773e50100c967887089d42bb1e281276
aishah2135 <37914729+aishah2135@users.noreply.github.com>
2024-03-31 19:36:30 -0400

    Add files via upload

---
+++ odd or even.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  5 21:06:01 2024
+
+@author: aisha
+"""
+
+number = int(input())
+
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This number is odd.")
